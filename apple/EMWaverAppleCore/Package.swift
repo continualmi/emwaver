e3c54407b55c90212eb21ba793d9db3915de1da5
Luís Lopes <maarnotto@gmail.com>
2026-01-31 09:22:30 +0000

    switch chart librarry

--- apple/EMWaverAppleCore/Package.swift
+++ apple/EMWaverAppleCore/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "EMWaverAppleCore",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v16),
         .macOS(.v13),
     ],
     products: [
