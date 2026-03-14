a53ebc419b6de686734b0516a06254ae46459842
luispl77 <maarnotto@gmail.com>
2026-03-14 13:17:24 +0000

    Add standalone ESP32-S3 blink firmware

---
+++ esp/blink/main/main.c
@@ -0,0 +1,61 @@
+/*
+ * EMWaver blink test firmware
+ * Copyright (c) 2026 Luís Marnoto
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+
+#ifndef CONFIG_BLINK_GPIO
+#define CONFIG_BLINK_GPIO 36
+#endif
+
+#ifndef CONFIG_BLINK_PERIOD
+#define CONFIG_BLINK_PERIOD 1000
+#endif
+
+#define BLINK_GPIO CONFIG_BLINK_GPIO
+
+static const char *TAG = "example";
+static uint8_t s_led_state = 0;
+
+static void blink_led(void)
+{
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink GPIO LED on GPIO %d", BLINK_GPIO);
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
+
+void app_main(void)
+{
+    configure_led();
+
+    for (;;) {
+        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state ? "ON" : "OFF");
+        blink_led();
+        s_led_state = !s_led_state;
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+    }
+}
